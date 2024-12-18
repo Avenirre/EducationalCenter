44ac4d8341696886f1f74e733785d32a44c01642
RomanVinokurov <avenirre@gmail.com>
2024-12-18 11:09:20 +0200

    init commit

---
+++ src/main/kotlin/com/kotlin/educationalcenter/EducationalCenterApplication.kt
@@ -0,0 +1,11 @@
+package com.kotlin.educationalcenter
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EducationalCenterApplication
+
+fun main(args: Array<String>) {
+    runApplication<EducationalCenterApplication>(*args)
+}
